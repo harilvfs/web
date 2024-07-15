e80e827c31c7fdfcd28abea0204b411c9a2d8a97
aayush <147874416+aayushx402@users.noreply.github.com>
2024-07-15 12:59:19 +0545

    update

--- src/components/TypeWriter.js
+++ src/components/TypeWriter.js
@@ -2,24 +2,24 @@ import Typed from "typed.js";
 import React, { useEffect, useRef } from "react";
 
 const TypeWriter = () => {
-  const el = useRef(null);
+    const el = useRef(null);
 
-  useEffect(() => {
-    const typed = new Typed(el.current, {
-      strings: ["Web Developer.", "Photographer.", "Designer."],
-      startDelay: 300,
-      typeSpeed: 100,
-      backSpeed: 25,
-      backDelay: 500,
-      loop: true,
-    });
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ["Linux Developer.", "Photographer.", "R & D."],
+            startDelay: 300,
+            typeSpeed: 100,
+            backSpeed: 25,
+            backDelay: 500,
+            loop: true,
+        });
 
-    return () => {
-      typed.destroy();
-    };
-  }, []);
+        return () => {
+            typed.destroy();
+        };
+    }, []);
 
-  return <span ref={el}></span>;
+    return <span ref = { el } > < /span>;
 };
 
 export default TypeWriter;
